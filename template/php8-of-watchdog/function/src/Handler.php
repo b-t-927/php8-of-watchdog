40d0139566a6caeec24fdf3ac1144a834d9289a7
bruceigt <bruce@brucedev.com>
2024-10-01 02:36:24 +0100

    Added Handler base file

---
+++ template/php8-of-watchdog/function/src/Handler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Handler
+ * @package App
+ */
+class Handler
+{
+    /**
+     * @return string|null
+     */
+    public function handle(): ?string
+    {
+        return;
+    }
+}
