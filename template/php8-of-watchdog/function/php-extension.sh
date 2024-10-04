92b8e23ea88c9b86fa0abc28370d835938af96ef
b-t-927 <bruce@brucedev.com>
2024-10-04 04:23:09 +0100

    Updated URL

--- template/php8-of-watchdog/function/php-extension.sh
+++ template/php8-of-watchdog/function/php-extension.sh
@@ -5,6 +5,6 @@ echo "Installing PHP extensions"
 # Add your extensions here, you can find a valid example in php-extension.example.sh
 #
 # See the template documentation for instructions on installing extensions;
-# - https://github.com/openfaas/templates/tree/master/template/php8
+# - https://github.com/b-t-927/php8-of-watchdog
 #
 # You can also install any apk packages here
